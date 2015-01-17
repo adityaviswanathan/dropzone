5616bfc86698b93fd6f5f9b6cda99ee3909c9fc5
Sam Redmond <sredmond@stanford.edu>
2015-01-17 14:01:46 -0800

    Rando config

--- run.py
+++ run.py
@@ -3,9 +3,10 @@ from server import app
 
 def runserver():
 	heroku = int(os.environ.get('HEROKU', 0))
+	print heroku
 	if (heroku != 0):
 		print "On Heroku"
-		app.run()
+		app.run(host='0.0.0.0')
 	else:
 		print "NOT on Heroku"
 		port = int(os.environ.get('PORT', 5000))
