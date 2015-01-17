e2c080ea0d9547d84b5daee6aa822f08a1da56cd
Aditya Viswanathan <aditya@adityaviswanathan.com>
2015-01-17 01:45:29 -0500

    added to drops to models

--- server/models.py
+++ server/models.py
@@ -1,6 +1,6 @@
 from . import app
 from server.db import db
-from sqlalchemy import ForeignKey
+from sqlalchemy import ForeignKey, Enum
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy.ext.declarative import declarative_base
 import datetime
@@ -28,6 +28,8 @@ class User(Base):
     name = db.Column(db.Text)
     email = db.Column(db.Text)
     photo = db.Column(db.Text)
+    drops = relationship('Drop', backref='users')
+    # founds = relationship('Drop')
 
     @staticmethod
     def query_by_user_id(user_id):
@@ -40,3 +42,14 @@ class User(Base):
         if email is None:
             return None
         return User.query.filter_by(email=email).first()
+
+class Drop(Base):
+    __tablename__ = 'drops'
+    data_type = db.Column(db.Enum('text','photo','video', name='data_types'))
+    data_payload = db.Column(db.Text)
+    numviews = db.Column(db.Integer)
+    restrictions = db.Column(db.Enum('self', 'friends', 'public', name='restriction_types'))
+    viewcap = db.Column(db.Integer)
+    # finders = relationship('User')
+    user_id = db.Column(db.Integer, ForeignKey('users.id'))
+    teaser = db.Column(db.Text)
