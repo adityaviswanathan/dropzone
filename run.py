8b5490e8d0c3726655f48cad91aa84ce6f1a2760
Sam Redmond <sredmond@stanford.edu>
2015-01-17 13:56:26 -0800

    Adds gitignore, run for heroku

--- run.py
+++ run.py
@@ -1,9 +1,15 @@
 import os
 from server import app
 
-def runserver():	
-	port = int(os.environ.get('PORT', 5000))
-	app.run(host='0.0.0.0', port=port)
-	
+def runserver():
+	heroku = int(os.environ.get('HEROKU', 0))
+	if (heroku != 0):
+		print "On Heroku"
+		app.run()
+	else:
+		print "NOT on Heroku"
+		port = int(os.environ.get('PORT', 5000))
+		app.run(host='0.0.0.0', port=port)
+
 if __name__ == '__main__':
     runserver()
